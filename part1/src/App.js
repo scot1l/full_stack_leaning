ef6043a7c0e101085b6abd7292862c9c7850a6fb
scot1l <1487462433g@gmail.com>
2023-02-08 01:10:07 +0800

    finish  part2a

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,65 +1,52 @@
-import { useState } from 'react'
-
-const Title = () => (
-  <div>
-    give feedback
-  </div>
-)
-
-const Button = ({ butName, onClick }) => (
-  <button onClick={onClick}>
-    {butName}
-  </button>
-)
-
-
-const Statistics = ({ statName, data, algo }) => {
-  console.log(statName, data);
-  return (
-    <div>
-      {statName} {algo(data)}
-    </div>
-  )
-}
-
-const naN2Zero = (num) => isNaN(num) ? 0 : num
-
-const sum = (data) => data.reduce((a, b) => a + b)
-const avg = (data) => naN2Zero((data[0] * 1 + data[2] * (-1)) / sum(data))
-const positive = (data) => (naN2Zero((data[0] / sum(data))) * 100) + '%'
-const origin = (data) => data
-
-const DisplayStat = ({ scoreList }) => scoreList.every(review => review == 0)
-  ? (<div>No feedback given!</div>)
-  : (<div>
-    <Statistics statName={'good'} data={scoreList[0]} algo={origin} />
-    <Statistics statName={'neutral'} data={scoreList[1]} algo={origin} />
-    <Statistics statName={'bad'} data={scoreList[2]} algo={origin} />
-    <Statistics statName={'all'} data={scoreList} algo={sum} />
-    <Statistics statName={'avg'} data={scoreList} algo={avg} />
-    <Statistics statName={'positive'} data={scoreList} algo={positive} />
-  </div>)
-
+import Course from "./Course"
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-
-  const scoreList = [good, neutral, bad]
-
-  return (
-    <div>
-      <Title />
-      <br></br>
-      <Button butName={'good'} onClick={() => setGood(good + 1)} />
-      <Button butName={'neutral'} onClick={() => setNeutral(neutral + 1)} />
-      <Button butName={'bad'} onClick={() => setBad(bad + 1)} />
-      <DisplayStat scoreList={scoreList}/>
-    </div>
-  )
+  return courses.map(course => <Course course={course} />)
 }
 
 export default App
