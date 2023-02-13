767b27f883ba445a34dfd88725c66b47928f06c1
scot1l <1487462433g@gmail.com>
2023-02-13 23:49:04 +0800

    change for deploy in cloud

---
+++ part3/phonebook/build/precache-manifest.5e6aeb9569064f631be5553ec3acc5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5330b228bdbc078315c133bfd1f50df1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fca7e8440e8b3f61c5a",
+    "url": "/static/js/2.8d8f4284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d8f4284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e15c054d3e4cf438700",
+    "url": "/static/js/main.bd4d344e.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,19 +1,14 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
 
+const cors = require('cors')
 const morgan = require('morgan')
 morgan.token('body', (req, resp) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
@@ -107,7 +102,7 @@ const unknownEndPoint = (request, response) => {
 
 app.use(unknownEndPoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
